8a3dc17a80146eeb2be7b34960025ae6de770a63
ai5le <tommy@ai5le.com>
2017-06-22 02:30:19 -0700

    Update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,10 @@
-
-
-
-    // Add scrolling
+// Add scrolling
 $('.navbar a').addClass('page-scroll');
+
+
+
+$(window).scroll(function(e) {
+    oVal = ($(window).scrollTop() / 170);
+    $(".blur").css("opacity", oVal);
+
+});
