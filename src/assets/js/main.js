4160b388c0ea00dc74fd58b47d82c2d68e7a9f10
ai5le <tommy@ai5le.com>
2017-06-26 03:12:49 -0700

    updates

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,20 @@
-$(document).ready();
+searchVisible = 0;
+transparent = true;
 
 // Add scrolling
 $('.navbar a').addClass('page-scroll');
+
+
+$(document).scroll(function() {
+    if ($(this).scrollTop() > 260) {
+        $('nav').addClass('navbar-inverse');
+        $('nav').removeClass('navbar-transparent');
+    }
+});
+
+$(document).scroll(function() {
+    if ($(this).scrollTop() < 260) {
+        $('nav').addClass('navbar-transparent');
+        $('nav').removeClass('navbar-inverse');
+    }
+});
